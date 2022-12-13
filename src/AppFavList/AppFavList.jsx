f070fdb3c7eef590785cea31f061dba5448f69f5
JemiJe <jeremiah7480@gmail.com>
2022-12-14 00:32:45 +0200

    optionsUI obj fix3

--- src/AppFavList/AppFavList.jsx
+++ src/AppFavList/AppFavList.jsx
@@ -121,6 +121,15 @@ class AppFavList extends Component {
         
         const storage =  favStorage('favListStorage').get();
 
+        if( !storage.optionsUI ) {
+
+            storage.optionsUI = {
+                currentFolder: '',
+                displayMode: "default",
+                sort: "dateUp"
+            };
+        }
+
         let items = storage.items;
         const options = storage.optionsUI;
 
